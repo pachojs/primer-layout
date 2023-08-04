7bc7c7a5f76481b695191746b9d4b957a2c3d219
Francisco Bedoya <pacho2390@gmail.com>
2023-08-03 20:39:54 -0500

    versión inicial de layout

---
+++ main.js
@@ -0,0 +1,5 @@
+const navHome = document.querySelector(".header_nav_ul_li_home") 
+const navWe = document.querySelector(".header_nav_ul_li_we")            
+const navservices = document.querySelector(".header_nav_ul_li_services")
+const navNews = document.querySelector(".header_nav_ul_li_news")
+const navContact = document.querySelector(".header_nav_ul_li_contact")
