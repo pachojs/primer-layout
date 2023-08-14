75b375b5012689b343425347a73f16c368b64c4f
pacho <pacho2390@gmail.com>
2023-08-14 18:26:57 -0500

    creo sectión

--- main.js
+++ main.js
@@ -2,3 +2,4 @@ const navHome = document.querySelector(".header_nav_ul_li_home")
 const navWe = document.querySelector(".header_nav_ul_li_we")            
 const navservices = document.querySelector(".header_nav_ul_li_services")
 const navContact = document.querySelector(".header_nav_ul_li_contact")
+
