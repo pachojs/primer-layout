d461ebda7b3167833d42164c7e98cfb6dd34745e
pacho <pacho2390@gmail.com>
2023-10-10 12:34:56 -0500

    se hace un cambio sustancial en la pagina, se modifica todo el html, el css y se agrega un nuevo js

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-const navHome = document.querySelector(".header_nav_ul_li_home") 
-const navWe = document.querySelector(".header_nav_ul_li_we")            
-const navservices = document.querySelector(".header_nav_ul_li_services")
-const navContact = document.querySelector(".header_nav_ul_li_contact")
+const togglerButton = document.querySelector('.toggler-button')
+const navBarMenu = document.querySelector('.nav_ul')
+
+
+togglerButton.addEventListener('click', ()=>{
+    navBarMenu.classList.toggle('inactive')
+})
+
 
