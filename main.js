2ae1db8c0cb536d89ad1d83e78980c0fa460b1da
pacho <pacho2390@gmail.com>
2023-10-20 17:20:47 -0500

    creo la seccion de aprende a confeccionar con su archivo html y css

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
-const togglerButton = document.querySelector('.toggler-button')
-const navBarMenu = document.querySelector('.nav_ul')
+AOS.init()
 
+const togglerButton = document.querySelector('.toggler-button');
+const navBarMenu = document.querySelector('.nav_ul');
+const buttonCloseInitalView =  document.querySelector('.boton-vista-inicial');
+const initialView = document.querySelector('.contenedor-vista-inicial');
+const container1 = document.querySelector('.container1');
+const footer = document.getElementById('footer')
 
 togglerButton.addEventListener('click', ()=>{
     navBarMenu.classList.toggle('inactive')
 })
 
 
+buttonCloseInitalView.addEventListener('click', () =>{
+    initialView.classList.add('inactive');
+    container1.classList.remove('inactive');
+    footer.classList.remove('inactive');
+
+})
