a7c9abbe4b8de33c969c3ebe588227bf249757bd
pacho <pacho2390@gmail.com>
2023-10-23 16:09:16 -0500

    hago html y css para el login y el registro

--- main.js
+++ main.js
@@ -4,17 +4,37 @@ const togglerButton = document.querySelector('.toggler-button');
 const navBarMenu = document.querySelector('.nav_ul');
 const buttonCloseInitalView =  document.querySelector('.boton-vista-inicial');
 const initialView = document.querySelector('.contenedor-vista-inicial');
-const container1 = document.querySelector('.container1');
+const closerDiv = document.querySelector('.closerDiv');
 const footer = document.getElementById('footer')
 
+
+
+//Menu Scroll
+let ubicacionPrincipal = window.pageYOffset;
+
+window.addEventListener("scroll", function(){
+    // console.log(window.pageYOffset);
+    let desplazamientoActual = window.pageYOffset;
+
+    if (ubicacionPrincipal >= desplazamientoActual) {
+        document.getElementsByTagName("header")[0].style.top = "0px";        
+    }else{
+        document.getElementsByTagName("header")[0].style.top = "-100px"; 
+    };
+
+    ubicacionPrincipal = desplazamientoActual;
+});
+
+// menú hamburguesa
 togglerButton.addEventListener('click', ()=>{
     navBarMenu.classList.toggle('inactive')
 })
 
-
+// menú vista inicial
 buttonCloseInitalView.addEventListener('click', () =>{
     initialView.classList.add('inactive');
-    container1.classList.remove('inactive');
+    closerDiv.classList.remove('inactive');
     footer.classList.remove('inactive');
 
 })
+
